d7b2a9054d360cb90bdd251551fd9bab48af963d
Bruna Vieira <brunavieira.work@gmail.com>
2021-01-03 19:53:40 -0300

    lifting state up

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
             </button>
         );
     }
@@ -14,8 +17,26 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'x';
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
